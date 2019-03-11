e6d7a1f19ba6302debbfc68483093fcc8578891e
Michael Vinh Xuan Thanh <meta.orphic@gmail.com>
2019-03-10 23:16:28 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyzing the titanic disaster data set',
+    author='Michael Vinh Xuan Thanh',
+    license='MIT',
+)
